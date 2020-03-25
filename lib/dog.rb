6276099bddfb938231dae574012a3024c2936f6c
Daniel Sun <danielsun12345@gmail.com>
2020-03-25 22:57:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -82,7 +82,13 @@ class Dog
   end 
   
   def self.find_by_name
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+    SQL
     
+    DB[:conn].execute(sql)
     
     
     
