2814928da7d4cec19912535282bed109832fe620
Daniel Sun <danielsun12345@gmail.com>
2020-03-25 22:55:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -76,7 +76,7 @@ class Dog
     if dog 
       new_dog = self.new_from_db
     else 
-      new_dog = self.create(:name => name , :breed => breed)
+      new_dog = self.create({:name => name , :breed => breed})
     end 
       new_dog 
   end 
