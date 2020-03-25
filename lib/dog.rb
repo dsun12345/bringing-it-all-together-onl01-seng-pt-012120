eb39b88350b5604582be6c8ff8324b4b47c6835b
Daniel Sun <danielsun12345@gmail.com>
2020-03-25 22:40:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,18 @@ class Dog
      self.id ||= nil 
   end 
   
-  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE dogs IF NOT EXISTS (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT 
+      )
+    SQL
+    
+    DB[:conn].execute(sql)
+    
+  end 
   
   
   
