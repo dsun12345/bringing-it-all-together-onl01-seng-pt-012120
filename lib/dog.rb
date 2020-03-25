d036bbba230220a7ac40e28269d72e8090d09735
Daniel Sun <danielsun12345@gmail.com>
2020-03-25 22:37:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   attr_accessor :id, :name, :breed
   
   def initialize(attributes)
-     attributes.each do {|key, value| self.send (("#{key}"))} 
+     attributes.each do {|key, value| self.send (("#{key}="))} 
   end 
   
   
