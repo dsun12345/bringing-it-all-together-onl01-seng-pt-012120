75fde1d00b46bf6ea07171f58e8e41fd2ad2e5b8
Daniel Sun <danielsun12345@gmail.com>
2020-03-25 22:59:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -81,14 +81,15 @@ class Dog
       new_dog 
   end 
   
-  def self.find_by_name(dog)
+  def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
       FROM dogs
       WHERE name = ?
     SQL
     
-    dogs = DB[:conn].execute(sql, self.name)
+    DB[:conn].execute(sql, name).map do |row|
+      self.
   end 
   
 end 
