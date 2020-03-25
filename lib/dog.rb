0b05d8f52b20b0cafd459efaba0220e6b1b328ca
Daniel Sun <danielsun12345@gmail.com>
2020-03-25 22:44:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,7 +32,7 @@ class Dog
     SQL
     
     DB[:conn].execute(sql, self.name, self.breed)
-    @id = DB[:conn].execute("SELECT last_insert_rowid()")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")
   end 
   
   
